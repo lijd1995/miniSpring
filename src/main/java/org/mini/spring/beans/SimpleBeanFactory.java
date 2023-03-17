70132b0e5e68e95f9c8904e690f6b2835e39e822
李俊达 <lijunda04@58.com>
2023-03-17 19:42:20 +0800

    feature：对BeanDefinition的构造器和Setter进行注入处理操作

--- src/main/java/org/mini/spring/beans/SimpleBeanFactory.java
+++ src/main/java/org/mini/spring/beans/SimpleBeanFactory.java
@@ -1,5 +1,9 @@
 package org.mini.spring.beans;
 
+import com.sun.tools.javac.code.Attribute;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +24,98 @@ public class SimpleBeanFactory extends DefaultSingletonBeanRegistry implements B
 	public SimpleBeanFactory(){
 
 	}
+
+	private Object createBean( BeanDefinition beanDefinition ) {
+
+		Class<?> clz = null;
+		Object obj = null;
+		Constructor<?> con = null;
+
+		try{
+			clz = Class.forName( beanDefinition.getClassName() );
+			// 处理构造器参数
+			ArgumentValues constructorArgumentValues = beanDefinition.getConstructorArgumentValues();
+			// 如果有参数
+			if( !constructorArgumentValues.isEmpty() ) {
+				Class<?>[] paramTypes = new Class<?>[ constructorArgumentValues.getArgumentCount() ];
+				Object[] paramValues = new Object[ constructorArgumentValues.getArgumentCount() ];
+				// 对每一个参数，分数据类型分别处理
+				for( int i = 0; i < constructorArgumentValues.getArgumentCount(); i++ ){
+					ArgumentValue argumentValue = constructorArgumentValues.getIndexArgumentValue( i );
+					if ("String".equals(argumentValue.getType()) || "java.lang.String".equals(argumentValue.getType())) {
+						paramTypes[i] = String.class;
+						paramValues[i] = argumentValue.getValue();
+					}
+					else if ("Integer".equals(argumentValue.getType()) || "java.lang.Integer".equals(argumentValue.getType())) {
+						paramTypes[i] = Integer.class;
+						paramValues[i] = Integer.valueOf((String) argumentValue.getValue());
+					}
+					else if ("int".equals(argumentValue.getType())) {
+						paramTypes[i] = int.class;
+						paramValues[i] = Integer.valueOf((String) argumentValue.getValue()).intValue();
+					}
+					else {
+						paramTypes[i] = String.class;
+						paramValues[i] = argumentValue.getValue();
+					}
+				}
+				try{
+					// 根据参数的类型选择构造器，然后newInstance的时候将值传入进去
+					con = clz.getConstructor( paramTypes );
+					obj = con.newInstance( paramValues );
+				}catch( Exception ex ){
+
+				}
+			} else {
+				// 如果没有参数，直接创建实例
+				obj = clz.newInstance();
+			}
+		}
+		catch( Exception ex ){
+			ex.printStackTrace();
+		}
+
+		PropertyValues propertyValues = beanDefinition.getPropertyValues();
+		if( !propertyValues.isEmpty() ){
+			for( int i = 0; i < propertyValues.size(); i++ ) {
+				PropertyValue propertyValue = propertyValues.getPropertyValueList().get( i );
+				String type = propertyValue.getType();
+				String name = propertyValue.getName();
+				Object value = propertyValue.getValue();
+				Class<?>[] paramTypes = new Class<?>[ 1 ];
+				if ("String".equals(type) || "java.lang.String".equals(type)) {
+					paramTypes[0] = String.class;
+				}
+				else if ("Integer".equals(type) || "java.lang.Integer".equals(type)) {
+					paramTypes[0] = Integer.class;
+				}
+				else if ("int".equals(type)) {
+					paramTypes[0] = int.class;
+				}
+				else {
+					paramTypes[0] = String.class;
+				}
+
+				Object[] paramValues = new Object[1];
+				paramValues[0] = value;
+
+				String methodName = "set" + name.substring( 0, 1 ).toUpperCase() + name.substring( 1 );
+				Method method = null;
+				try{
+					method = clz.getMethod( methodName, paramTypes );
+					// 获取了构造器，就调用方法，将参数传入进去
+					method.invoke( obj, paramValues );
+				}
+				catch( Exception ex ){
+					ex.printStackTrace();
+				}
+			}
+		}
+
+		return obj;
+
+	}
+
 
 	/**
 	 * 根据 beanName 获取 Bean，改造后交给父类处理，父类处理单例
